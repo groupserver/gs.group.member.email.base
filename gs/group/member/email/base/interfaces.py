40cf6fa5d729769c7e383604017ad7b063f73ae5
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-04 12:52:55 -0700

    Adding an interface, and having a stab at making an adaptor.

---
+++ gs/group/member/email/base/interfaces.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import absolute_import, unicode_literals
+from zope.interface import Interface
+
+
+class IGroupEmailUser(Interface):
+    def get_delivery_setting():
+        'Get the delivery setting for the group member in the group.'
+
+    def get_addresses():
+        'Get the email addresses used by the group member in the group.'
